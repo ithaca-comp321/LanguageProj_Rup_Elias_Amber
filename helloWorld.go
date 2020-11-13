b7fb630a17a05710177812ff9e5ed10138e6f0ad
thegoat <ruppatel11599@gmail.com>
2020-11-13 12:44:26 -0500

    comments on hangman game

--- helloWorld.go
+++ helloWorld.go
@@ -1,5 +1,44 @@
 package main
 import "fmt"
-func main() {
-    fmt.Println("hello world")
+
+
+
+func insertLetter(letter){
+    //string formatting
+
+
 }
+
+
+func drawBodyPart(count){
+    //list of stuff head, left arm, right arm, left leg, right leg, body, 
+
+
+}
+
+func addGuessToSet(letter){
+    //when letters is guessed then automatically add to set
+    //if it already exists then return "enter another letter"
+
+
+
+}
+
+
+
+func main() {
+
+    //create a list of all the words that the player will guess
+
+    //randomly select word user has to guess and replace letters with '_'
+
+    //if they get the letter right, prompt to guess whole word, if wrong then add a body part
+
+
+    //create an empty set to hold all guessed letters that user entered
+    
+
+
+}
+
+
