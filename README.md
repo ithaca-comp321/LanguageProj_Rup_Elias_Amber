bdeb51d94bc10d96fd3dafa103281743d33d2376
Elias <elias@jwplatt.com>
2020-11-18 21:54:09 -0500

    wrote readme, made starting test file, fixed and cleaned up hangMan.go, some more function progress

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# LanguageProj_Rup_Elias_Amber
+# COMP 321 project 4 part 1 - starter program
+Code Authors: Amber, Elias, and Rup.
+####Purpose:
+This basic Go program mimics the popular Hangman game where users guess the letters of a word
+and try to reveal the whole word before making too many wrong guesses.
+
+####Instructions for running:
+This program can be freely run with VSCode or GoLand, a Jetbrains IDE. In either case, you will first need to
+install Go from here: https://golang.org/doc/install.
+
+For GoLand, read through https://www.jetbrains.com/help/go/quick-start-guide-goland.html and clone
+the repository. If it's not working, you may need to configure the GOROOT and GOPATH - https://www.jetbrains.com/help/go/configuring-goroot-and-gopath.html
+
+
+For VSCode, install the Go extension and clone the repository.
+
+Documentation and guides for using Go can be found elsewhere in the websites linked above. 
+Useful packages can be found at https://pkg.go.dev/. Also, an external tutorial 
+site may be useful: https://www.tutorialspoint.com/go.
+
+####Notes:
+Our initial investigation of Go: 
+https://docs.google.com/document/d/1fop2oMv6MdyyNyQqXiKQ_ZE6mseTU5dZPOyg3uXcqp4
--- hangMan.go
+++ hangMan.go
@@ -2,75 +2,85 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	//"regexp"
 )
 
-func insertLetter() {
-	//string formatting
+var bodyPart = []string{"head", "left arm", "right arm", "left leg", "right leg", "body"}
+var words = []string{"bananas", "apples", "peach", "elephant", "orange", "burritos", "ithaca college"}
 
+var currentBody []string
+var guesses []string //create an empty set to hold all guessed letters that user entered
+var word string
+
+func drawBodyPart(wrongGuessCount int) []string {
+	for i := 0; i <= wrongGuessCount; i++ {
+		currentBody = append(currentBody, bodyPart[i])
+	}
+	return currentBody
 }
 
-func drawBodyPart(int count) {
-	//list of stuff head, left arm, right arm, left leg, right leg, body,
-    var bodyPart = []string {"head", "left arm", "right arm", "left leg", "right leg", "body"}
-    	
-	for i = 0; bodyPart <= 0; i++ {
-		bodyPart = bodyPart[i-1];
-		return bodyPart;
-    }
-    
-}
-
-func addGuessToSet() {
+func addGuessToSet(guess string) string {
 	//when letters is guessed then automatically add to set
 	//if it already exists then return "enter another letter"
-
+	var duplicate bool = false
+	for _, i := range guesses {
+		if i == guess {
+			duplicate = true
+		}
+	}
+	if duplicate {
+		return "You guessed a duplicate letter, guess another.\n"
+	} else {
+		guesses = append(guesses, guess)
+		return "You guessed " + guess + "\n"
+	}
 }
 
-func chooseRandomWord(words) {
-	////DOESN'T WORK
-	var randomWord = rand.string(len(words))
+func chooseRandomWord() string {
+	var randomWord = rand.Intn(len(words))
 
-	pick := words[randomWord]
-	return
+	var pick string = words[randomWord]
+	return pick
+}
+
+func getWordProgress() (string, bool) { //TODO
+	//init wordProgress with a string of underscores of length that matches the word
+	//look through guesses - if a guessed letter is in word, then look for where it exists in the word and
+	//replace the respective underscores in wordProgress. Might want to make this a set?
+	//return wordProgress and if the guessed letter was in the word or not
+	//regexp package may be useful?
+
+	return "", false
 }
 
 func main() {
 
-    //if they get the letter right, prompt to guess whole word, if wrong then add a body part
+	var wrongGuessCount int
+	var answer string
 
-    fmt.Println("Enter a letter you will let to guess: ") 
-  
-    var guessLetter string 
-    fmt.Scanf("%s",&guessLetter)
+	word = chooseRandomWord() //randomly select word user has to guess
+	fmt.Println("Your word is :  ")
+	getWordProgress()
 
-    if guessLetter == ...{
-        fmt.Println("Do you wan a letter you will let to guess: ");
-        var answer string 
-        fmt.Scanf("%s",&answer) 
-        if answer =="yes"{
-            
-        }
-        
-    }
-    
+	for answer != "quit" {
+		//TODO Ask if they want to guess a letter or the whole word, then prompt for it
+		fmt.Scanf("%s", &answer)
 
-	//create a list of all the words that the player will guess
+		if answer == "letter" {
+			fmt.Println("\nGuess a letter or enter 'quit' to stop: ")
+			fmt.Scanf("%s", &answer) //TODO need to do some user error checking here, make sure they only enter a letter
 
-	//get the words inside of our defined list
-	var words []string
-	words = append(words, "bananas")
-	words = append(words, "apples")
-	words = append(words, "peach")
-	words = append(words, "elephant")
-	words = append(words, "orange")
-	words = append(words, "burritos")
+			addGuessToSet(answer)
+			wordProgress, guessedRight := getWordProgress()
 
-	//this is how we can print the whole list
-	fmt.Printf("%v", words)
+			if !guessedRight {
+				wrongGuessCount++
+				currentBody = drawBodyPart(wrongGuessCount)
+			}
 
+			fmt.Println("Word Progress: " + wordProgress)
+			fmt.Printf("Bodyparts visible: %v", currentBody) //this prints the whole list?
+		}
+	}
 }
-
-//randomly select word user has to guess and replace letters with '_'
-
-
-//create an empty set to hold all guessed letters that user entered
